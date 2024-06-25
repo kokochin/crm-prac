abace7c23991042bacf8194e147962b58bd08be3
ctw88 <rikuto.hirao0905@gmail.com>
2024-06-25 10:00:46 +0900

    sec05-68-77

--- src/resources/js/Pages/Purchases/Create.vue
+++ src/resources/js/Pages/Purchases/Create.vue
@@ -1,4 +1,6 @@
 <script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
 import { getToday } from '@/common'
 import { onMounted, reactive, ref, computed } from 'vue'
 import { Inertia  } from '@inertiajs/inertia';
