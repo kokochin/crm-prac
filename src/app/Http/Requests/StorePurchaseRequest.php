21d5d00712669acfbcd655784b3fb1ea507e92af
ctw88 <rikuto.hirao0905@gmail.com>
2024-06-25 09:32:13 +0900

    sec05-68-77

--- src/app/Http/Requests/StorePurchaseRequest.php
+++ src/app/Http/Requests/StorePurchaseRequest.php
@@ -9,9 +9,9 @@ class StorePurchaseRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -19,10 +19,10 @@ class StorePurchaseRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'cusutomer_id' => [ 'required']
         ];
     }
 }
